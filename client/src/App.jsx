bcb26abc0b8022745f741a41f37eb66e9727f8bc
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-09-30 16:09:35 +0300

    upd: Client files formatted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <div className='App'><h1>Hello World!</h1></div>
+  return (
+    <div className='App'>
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
 
 export default App
