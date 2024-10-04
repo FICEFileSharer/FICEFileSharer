2b1f708746ddcefa552ab28fd11234d52b4a743c
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-04 21:11:49 +0300

    add: User Service with Mongo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserModule } from './users/user.module'
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { ConfigModule } from '@nestjs/config'
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_LOCAL_CONNECTION),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { UserDTO } from 'src/dto/user.dto'
+import { User } from 'src/schemas/User.schema'
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private user: Model<User>) {}
+  create = (userDTO: UserDTO) => new this.user(userDTO).save()
+}
