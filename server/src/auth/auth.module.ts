0e57ad7109d4403c2148dcde1428def4d03ffa6e
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-04 21:11:23 +0300

    semi: Started working with Auth Service

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { GoogleStrategy } from './utils/GoogleStrategy'
 import GoogleAuthGuard from './utils/GoogleAuthGuard'
+import { User, UserSchema } from 'src/schemas/User.schema'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, GoogleAuthGuard],
+  providers: [
+    AuthService,
+    GoogleStrategy,
+    {
+      provide: 'AUTH_SERVICE',
+      useClass: AuthService,
+    },
+    GoogleAuthGuard,
+  ],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { User } from 'src/schemas/User.schema'
+import { Model } from 'mongoose'
+import { UserDTO } from 'src/dto/user.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private user: Model<User>) {}
+
+  validateUser = (UserDTO: UserDTO) => {}
+}
