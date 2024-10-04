e7121bdfa790d02cfa5a07b3904564bef4d678c9
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-04 13:16:07 +0300

    upd: Invoking of Google oauth

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { GoogleStrategy } from './utils/GoogleStrategy'
+import GoogleAuthGuard from './utils/GoogleAuthGuard'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy, GoogleAuthGuard],
 })
 export class AuthModule {}
