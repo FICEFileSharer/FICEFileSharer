818ed2ead8ca141fe08cf20402326748c06a4b60
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-10-27 16:41:50 +0200

    upd: top scroll button

--- client/src/components/buttons/TopScrollButton/TopScrollButton.jsx
+++ client/src/components/buttons/TopScrollButton/TopScrollButton.jsx
@@ -25,7 +25,7 @@ const TopScrollButton = () => {
             className={`scroll-to-top position-fixed d-flex align-items-center rounded-circle fs-2 ${isVisible ? 'd-inline-block' : 'd-none'}`}
             onClick={scrollToTop}
         >
-            <i className='bi bi-arrow-up-circle bottom-mb bottom-10 end-2 position-fixed'></i>
+            <i className='bi bi-arrow-up-circle bottom-mb end-pc bottom-10 end-2 position-fixed'></i>
         </div>
     );
 };
